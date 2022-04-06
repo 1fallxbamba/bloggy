3f77ab4d5cd2f0ffa07d0a43d12312292e256fb2
1fallxbamba <fbamba99@gmail.com>
2022-04-06 12:03:04 +0000

    ++: handler for registration button

--- src/main/java/com/bloggy/bloggy/pages/authentication/registration/RegistrationController.java
+++ src/main/java/com/bloggy/bloggy/pages/authentication/registration/RegistrationController.java
@@ -1,11 +1,18 @@
 package com.bloggy.bloggy.pages.authentication.registration;
 
+import com.bloggy.bloggy.helpers.Alerter;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.IOException;
 
@@ -14,7 +21,26 @@ public class RegistrationController {
     private Stage stage;
     private Scene scene;
 
-    public void handleRegisterButton(ActionEvent actionEvent) {
+    @FXML
+    private TextField fullNameField;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button registerButton;
+
+
+    public void handleRegister(ActionEvent actionEvent) {
+
+        if(fullNameField.getText().isEmpty() || usernameField.getText().isEmpty() || passwordField.getText().isEmpty()) {
+            Alerter.showMessage("Invalid form","Fill all the fields man !", "how do you expect us to create your account ?...");
+        } else {
+        }
+
     }
 
     public void navigateToLogin(ActionEvent event) throws IOException {
